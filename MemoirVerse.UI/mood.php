756b1caa802259d2ef895f2e2e355886a0fa50e9
ILAGAN, ANDREI JIREH M. <ilaganandreijireh@gmail.com>
2024-06-15 14:48:21 +0800

    add necessary spacings for readability

--- MemoirVerse.UI/mood.php
+++ MemoirVerse.UI/mood.php
@@ -39,6 +39,7 @@ $sql = "INSERT INTO moods (mood) VALUES ('$mood')
              <div class="profile-pic"></div>
             <span class="profile-name">Memories</span>
         </div>
+
         <div class="menu">
             <div class="menu-item">Home</div>
             <div class="menu-item active">Mood Tracker</div>
@@ -46,10 +47,12 @@ $sql = "INSERT INTO moods (mood) VALUES ('$mood')
             <div class="menu-item">Pinned</div>
         </div>
     </div>
+
     <div class="main">
         <div class="header">
             <input type="text" class="search-bar" placeholder="Sorting" />
         </div>
+
         <div class="content">
             <div class="mood-tracker">
                 <h2>Choose Your State of Mind:</h2>
@@ -57,22 +60,28 @@ $sql = "INSERT INTO moods (mood) VALUES ('$mood')
                     <div class="mood-icon" data-mood="happy">
                         <img src="path_to_happy_icon.png" alt="Happy" />
                     </div>
+
                     <div class="mood-icon" data-mood="content">
                         <img src="path_to_content_icon.png" alt="Content" />
                     </div>
+
                     <div class="mood-icon" data-mood="neutral">
                         <img src="path_to_neutral_icon.png" alt="Neutral" />
                     </div>
+
                     <div class="mood-icon" data-mood="sad">
                         <img src="path_to_sad_icon.png" alt="Sad" />
                     </div>
+                    
                     <div class="mood-icon" data-mood="angry">
                         <img src="path_to_angry_icon.png" alt="Angry" />
                     </div>
-                    </div>
+                </div>
+
                 <div class="motivational-quote">Motivational Quote</div>
             </div>
         </div>
+
         <div class="footer">
             <button class="more-pages-button">More Pages</button>
             <div class="button-container">
@@ -80,6 +89,7 @@ $sql = "INSERT INTO moods (mood) VALUES ('$mood')
                     <button type="submit" class="home-btn">Home Page</button>
                  </a>
             </div>
+            
             <form action="login.php" method="post">
                 <button type="submit" class="logout-btn">Logout</button>
             </form>
