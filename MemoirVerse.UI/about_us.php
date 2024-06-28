eccfdb009be883c807f7ce0e20f3c6a80d38265b
Angelica G. Rosario <angelicalykarosario@gmail.com>
2024-06-29 04:19:35 +0800

    update and add new chnages to about us page and to_do_list.css

--- MemoirVerse.UI/about_us.php
+++ MemoirVerse.UI/about_us.php
@@ -1,89 +1,116 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>About Us</title>
-  <link rel="stylesheet" href="./style/about_us.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="./style/about_us.css">
 </head>
 <body>
-      <div class="person">
-      <div class="container">
-        <div class="container-inner">
-          <img class="circle" src="./assets/armedilla.jpg"/>
-          <img class="img img1" src=""/>
-        </div>
-      </div>
+   <header>
+       <div class="header-content">
+          <h1>MemoirVerse</h1>
+            <nav>
+               <ul>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="#contact">Contact Info</a></li>
+                <li><a href="#team">Meet the Team</a></li>
+               </ul>
+            </nav>
+       </div>
+   </header>
 
-      <div class="divider"></div>
-      <div class="name">Armedilla, Alliyah Samantha D.</div>
-      <div class="title"></div>
-      </div>
+    <main>
+        <section id="about" class="section about">
+            <div class="section-content">
+                <h2>Our Website</h2>
+                <p>Vision: To be the nation's best diary system.<br>
+                    Mission: To be the most user-friendly diary 
+                    system available to everyone.<br>
+                    
+                    MemoirVerse is a start up diary website created to
+                    have your own personal space and share your thoughts
+                    and feelings as we care for you. <br>
+                    
+                    This website was created and developed by 6 students
+                    serving as a school project turning to be one their
+                    greatest works as they push through in this industry.</p>
+            </div>
+        </section>
 
-      <div class="person">
-      <div class="container">
-        <div class="container-inner">
-          <img class="circle" src="./assets/escuro.jpg"/>
-          <img class="img img2" src="" />
-        </div>
+        <section id="contact" class="section contact">
+            <div class="section-content">
+                <h2>Contact Info</h2>
+                <p>Email: theteam.sixthsense@gmail.com</p>
+                <p>Phone: 09916277362</p>
+                <p>Address: Taguig City</p>
+            </div>
+        </section>
 
-      </div>
-      <div class="divider"></div>
-      <div class="name">Escuro, John Deniel L.</div>
-      <div class="title"></div>
-      </div>
+        <section id="team" class="section team">
+            <div class="section-content">
+                <h2>Meet the Team</h2>
+                <p>Our team of Polytechnic University of the Philippines 
+                   - Taguig students strive to provide the best service.</p>
+            </div>
+            <div class="members">
+                <div id="alliyah" class="member">
+                   <div class="member-info">
+                      <img src="./assets/armedilla.jpg" 
+                        alt="Alliyah Samantha Armedilla">
+                        <h3>Alliyah Samantha Armedilla</h3>
+                        <p>UI/UX Designer | Developer</p>
+                   </div>
+                </div>
 
-      <div class="person">
-      <div class="container">
-        <div class="container-inner">
-          <img
-            class="circle" src="./assets/gasta.jpg"/>
-          <img class="img img3" src=""/>
-        </div>
-      </div>
+                <div id="jd" class="member">
+                   <div class="member-info">
+                     <img src="./assets/escuro.jpg" 
+                       alt="John Deniel Escuro">
+                       <h3>John Deniel Escuro</h3>
+                       <p>Project Manager | Developer</p>
+                   </div>
+                </div>
 
-      <div class="divider"></div>
-      <div class="name">Gasta, Marc Oliver L.</div>
-      <div class="title"></div>
-      </div>
+                <div id="marc" class="member">
+                   <div class="member-info">
+                     <img src="./assets/gasta.jpg" 
+                        alt="Marc Oliver Gasta">
+                        <h3>Marc Oliver Gasta</h3>
+                        <p>Technical Lead | Developer</p>
+                   </div>
+                </div>
 
-      <div class="person">
-        <div class="container">
-          <div class="container-inner">
-            <img class="circle" src="./assets/ilagan.jpg"/>
-            <img class="img img4" src=""/>
-          </div>
-        </div>
+                <div id="jai" class="member">
+                   <div class="member-info">
+                      <img src="./assets/ilagan.jpg" 
+                        alt="Andrei Jireh Ilagan">
+                        <h3>Andrei Jireh Ilagan</h3>
+                        <p>Tester | Developer</p>
+                   </div>
+                </div>
 
-        <div class="divider"></div>
-        <div class="name">Ilagan, Andrei Jireh M.</div>
-        <div class="title"></div>
-      </div>
+                <div id="angelica" class="member">
+                   <div class="member-info">
+                     <img src="./assets/rosario.jpg" 
+                        alt="Angelica Rosario">
+                        <h3>Angelica Rosario</h3>
+                        <p>UI/UX Designer | Developer</p>
+                   </div>
+                </div>
 
-      <div class="person">
-        <div class="container">
-          <div class="container-inner">
-            <img class="circle"src="./assets/rosario.jpg"/>
-            <img class="img img5" src=""/>
-          </div>
-        </div>
+                <div id="kian" class="member">
+                   <div class="member-info">
+                      <img src="./assests/trinidad.jpg" 
+                        alt="Kian Lee Trinidad">
+                        <h3>Kian Lee Trinidad</h3>
+                        <p>Project Manager | Tester | Developer</p>
+                   </div>
+                </div>
+            </div>
+        </section>
+    </main>
 
-        <div class="divider"></div>
-        <div class="name">Rosario, Angelica G.</div>
-        <div class="title"></div>
-      </div>
-
-      <div class="person">
-        <div class="container">
-          <div class="container-inner">
-            <img class="circle" src="./assets/trinidad.jpg"/>
-            <img class="img img6" src=""/>
-          </div>
-        </div>
-
-        <div class="divider"></div>
-        <div class="name">Trinidad, Kian Lee B.</div>
-        <div class="title"></div>
-      </div>
+    <script src="./script/about_us.js"></script>
 </body>
 </html>
