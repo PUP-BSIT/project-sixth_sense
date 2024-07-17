699966a5f91817fba100ffa5184a6d0769510267
Trinidad, Kian Lee B. <trinidadkianleeb@gmail.com>
2024-07-17 21:38:22 +0800

    modify servername

--- MemoirVerse.UI/get_letters.php
+++ MemoirVerse.UI/get_letters.php
@@ -1,13 +1,14 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "memoirverse1";
+$servername = "localhost"; 
+$username = "u586757316_root";  
+$password = "";  
+$dbname = "memoirverse1";  
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+
+$conn = new mysqli('localhost', 'u586757316_root', 'Sixthsense21', 'u586757316_memoirverse1');
 
 if ($conn->connect_error) {
-    die(json_encode(["success" => false, "error" => "Connection failed: " . $conn->connect_error]));
+    die("Connection failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT title, content, created_at FROM letters ORDER BY created_at DESC";
