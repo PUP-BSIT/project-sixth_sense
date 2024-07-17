13bc20115608e04c3621bb67ae66fe4a583a4908
nukoww <johndenielescuro@gmail.com>
2024-07-17 18:16:54 +0800

    add changes for hostinger

--- MemoirVerse.UI/db_conn.php
+++ MemoirVerse.UI/db_conn.php
@@ -1,11 +1,12 @@
 <?php
 
 $servername = "localhost"; 
-$username = "root";  
+$username = "u586757316_root";  
 $password = "";  
 $dbname = "memoirverse1";  
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+
+$conn = new mysqli('localhost', 'u586757316_root', 'Sixthsense21', 'u586757316_memoirverse1');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -13,4 +14,8 @@ if ($conn->connect_error) {
 
 $conn->set_charset("utf8mb4");
 
+function sanitize_input($data) {
+    global $conn;
+    return $conn->real_escape_string(trim($data));
+}
 ?>
