e8aca3f53419d16705a2b7046dc7d4d19ebce2d9
Gasta Marc Oliver <marcolivergastagonzales@gmail.com>
2024-07-19 16:26:54 +0800

    established correct connection to remote database

--- MemoirVerse.UI/to_do_list.php
+++ MemoirVerse.UI/to_do_list.php
@@ -1,9 +1,16 @@
 <?php
-if (!file_exists('db_conn.php')) {
-    die("Database connection file is missing.");
-}
+session_start();
 
-include 'db_conn.php';
+$servername = "localhost"; 
+$username = "u586757316_root";  
+$password = "";  
+$dbname = "memoirverse1";  
+
+$conn = new mysqli('localhost', 'u586757316_root', 'Sixthsense21', 'u586757316_memoirverse1');
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $input = json_decode(file_get_contents('php://input'), true);
